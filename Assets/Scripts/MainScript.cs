7d71e30dbb30662aa2d89521fb360c80ec3b66a0
Ivan Golubev <ivan.golubev.spb@gmail.com>
2017-05-18 22:02:23 +0100

    Adding the final dialogue
    Adding the final dialogue

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -10,57 +10,81 @@ using SimpleJSON;
 
 public class MainScript : MonoBehaviour {
 
-    public Text QuestionText, DashesText;
-    public Image HangmanImage;
+    public Text QuestionText, DashesText, ResultsText;
+    public Image HangmanImage, FinalImage;
     public Sprite[] HangmanSprites;
+    public Sprite WinSprite, LoseSprite;
+
+    public GameObject MainDialogue, FinalDialogue;
 
     private int currentHangmanSprite = 0;
     private const int TOTAL_HANGMAN_SPRITES = 8;
+    private const char PLACEHOLDER = '*';
 
     private Dictionary<string, string> gameDict;
-    private string answer;
+    private string answer, userInput;
 
 	void Start () {
         gameDict = new Dictionary<string, string>();
-        loadDictionary("FruitsDictionary", gameDict); // file should be without ext - thanks unity !
-        loadDictionary("SportsDictionary", gameDict);
-        pickRandomQuestion();
+        LoadDictionary("FruitsDictionary", gameDict); // file should be without ext - thanks unity !
+        LoadDictionary("SportsDictionary", gameDict);
+        PickRandomQuestion();
     }
 	
     public void OnRestartClicked() {
+        MainDialogue.SetActive(true);
+        FinalDialogue.SetActive(false);
         currentHangmanSprite = 0;
         HangmanImage.sprite = HangmanSprites[currentHangmanSprite];
-        pickRandomQuestion();
+        PickRandomQuestion();
     }
 
     public void OnGuessSubmitted(Button button) {
         char letter = button.GetComponentInChildren<Text>().text.ToCharArray()[0];
-        if (answer.Contains(letter)) {
-            Debug.Log("!");
+        if ( answer.Contains(letter) ) {
+            UpdateAnswerText(letter);
+            if ( CheckWinCondition() ) {
+                Debug.Log("You won the game !");
+                ShowFinalDialogue(true);
+            }
         }
         else
         {
-            if (CheckLoseCondition()) { Debug.Log("You lost the game"); }
+            if (CheckLoseCondition()) {
+                Debug.Log("You lost the game");
+                ShowFinalDialogue(false);
+            }
             else { DrawNextHangmanPart(); }
         }
     }
 
-    private void pickRandomQuestion()
+    private void PickRandomQuestion()
     {
         int randInt = Random.Range(0, gameDict.Count);
         QuestionText.text = gameDict.ElementAt(randInt).Key;
         answer = gameDict.ElementAt(randInt).Value.ToUpper();
         StringBuilder sb = new StringBuilder("");
-        for (int i = 0; i < answer.Length; i++) { sb.Append("_ "); }
+        for (int i = 0; i < answer.Length; i++) { sb.Append(PLACEHOLDER); }
         DashesText.text = sb.ToString();
-        Debug.Log("Answer " + answer);
+        userInput = sb.ToString();
+        Debug.Log("Answer: " + answer);
     }
 
-    private void loadDictionary(string dictFileName, Dictionary<string, string> outputDict)
+    private void LoadDictionary(string dictFileName, Dictionary<string, string> outputDict)
     {
         TextAsset ta = Resources.Load(dictFileName) as TextAsset;
         JSONObject jsonObj = (JSONObject)JSON.Parse(ta.text);
         foreach (var key in jsonObj.GetKeys()) { outputDict[key] = jsonObj[key]; }
+    }
+
+    private void UpdateAnswerText(char letter) {
+        char[] userInputArray = userInput.ToCharArray();
+        for (int i = 0; i < answer.Length; i++) {
+            if (userInputArray[i] != PLACEHOLDER) { continue; } // already guessed
+            if (answer[i] == letter) { userInputArray[i] = letter; }
+        }
+        userInput = new string(userInputArray);
+        DashesText.text = userInput;
     }
 
     private void DrawNextHangmanPart() {
@@ -68,8 +92,13 @@ public class MainScript : MonoBehaviour {
         HangmanImage.sprite = HangmanSprites[currentHangmanSprite];
     }
 
-    private bool CheckWinCondition() { return false; }
-
+    private bool CheckWinCondition() { return answer.Equals(userInput); }
     private bool CheckLoseCondition() { return currentHangmanSprite == TOTAL_HANGMAN_SPRITES-1; }
 
+    private void ShowFinalDialogue(bool win) {
+        MainDialogue.SetActive(false);
+        FinalDialogue.SetActive(true);
+        FinalImage.sprite = win ? WinSprite : LoseSprite;
+        ResultsText.text = win ? "Victory !" : "Defeat !!!";
+    }
 }
--- README.md
+++ README.md
@@ -4,14 +4,10 @@ Hangman game: description
 Hangman is a paper and pencil guessing game for two or more players.
 One player thinks of a word, phrase or sentence and the other tries to guess it by suggesting letters or numbers,
  within a certain number of guesses.
- 
-The word to guess is represented by a row of dashes, representing each letter of the word.
-If the guessing player suggests a letter which occurs in the word, the other player writes it in all its correct positions. 
-If the suggested letter or number does not occur in the word, the other player draws one element of a hanged man stick figure as a tally mark.
 
-The player guessing the word may, at any time, attempt to guess the whole word.
-If the word is correct, the game is over and the guesser wins. 
-Otherwise, the other player may choose to penalize the guesser by adding an element to the diagram.
+The word to guess is represented by a row of dashes, representing each letter of the word.
+If the guessing player suggests a letter which occurs in the word, the other player writes it in all its correct positions.
+If the suggested letter or number does not occur in the word, the other player draws one element of a hanged man stick figure as a tally mark.
 
 Dictionaries:
 ------------
@@ -19,16 +15,16 @@ Dictionaries:
 1. Fruits
 2. Sports
 
-Questions and answers are stored in JSON 
-{"Question" : "Answer", ...} 
-format in separate files: Assets/Dictionaries/Fruits.json and Assets/Dictionaries/Sports.json.
+Questions and answers are stored in JSON
+{"Question" : "Answer", ...}
+format in separate files: Assets/Resources/FruitsDictionary.json and Assets/Resources/SportsDictionary.json.
 
 Rules:
 -------
 user can make up to MAX_ERRORS=7 mistakes before hangman diagram is complete.
-Word is selected at random  from the input dictionary according to the chosen topic.
+Word is selected at random from the input dictionaries.
 
-Art:
+Pictures:
 ----
 
 Simple 2D images:
